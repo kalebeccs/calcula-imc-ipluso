d9b6e78f3dbe1c9ca1f0c99b5a155e7ebc508bc4
kalebeccs <kalebe.correa.santos@gmail.com>
2024-11-27 17:48:14 +0000

    Update users.py
    added function to classify bmi

--- users.py
+++ users.py
@@ -32,3 +32,17 @@ def consulta_por_nome(cursor, nome_pesquisa):
 
 def calcula_IMC(peso, altura):
     return peso / altura ** 2
+
+def classificacao_IMC(imc):
+    if imc < 18.5:
+        return 'Abaixo do peso'
+    elif 18.5 <= imc < 25:
+        return 'Peso normal'
+    elif 25 <= imc < 30:
+        return 'Sobrepeso'
+    elif 30 <= imc < 35:
+        return 'Obesidade Grau 1'
+    elif 35 <= imc < 40:
+        return 'Obesidade Grau 2'
+    else:
+        return 'Obesidade Grau 3'
