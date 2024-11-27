a5f69c88b81e6cda84e72898739d4c4b3580ad92
kalebeccs <kalebe.correa.santos@gmail.com>
2024-11-27 19:40:29 +0000

    Update db.py
    adding error handling

--- db/db.py
+++ db/db.py
@@ -1,9 +1,26 @@
 import sqlite3
 
-_conn = sqlite3.connect('../db/imc.db')
+# Conexão inicial
+try:
+    _conn = sqlite3.connect('../db/imc.db')
+except sqlite3.Error as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
+    _conn = None
 
 def conn():
-    return _conn
+    """
+    Retorna a conexão com o banco de dados.
+    """
+    if _conn:
+        return _conn
+    else:
+        raise ConnectionError("Conexão com o banco de dados não está disponível.")
 
 def close():
-    _conn.close()
+    """
+    Fecha a conexão com o banco de dados.
+    """
+    if _conn:
+        _conn.close()
+    else:
+        print("Conexão já estava fechada.")
